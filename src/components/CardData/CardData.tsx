611d9048af8c52502ca7f21f105c735d6a3eda67
Oleksandr-Berd <alex.berd86@gmail.com>
2025-07-05 08:16:19 +0300

    basic markup for card form is made

--- src/components/CardData/CardData.tsx
+++ src/components/CardData/CardData.tsx
@@ -19,16 +19,57 @@ const CardData: React.FC = () => {
     },
   });
 
-  const handleSubmit = (evt:FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     console.log(formik.values);
   };
 
-  return <SC.CardDataCon>
-    <form action="submi" onSubmit={handleSubmit}>
-        
-    </form>
-  </SC.CardDataCon>;
+  return (
+    <SC.CardDataCon>
+      <form action="submi" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="holderName">
+            <span>cardholder name</span>
+            <input
+              type="text"
+              name="holderName"
+              id="holderName"
+              placeholder="e.g. Jane Appleseed"
+            />
+          </label>
+          <div>
+            <label htmlFor="cardNumber">
+              <span>card number</span>
+              <input
+                type="text"
+                name="cardNumber"
+                placeholder="e.g. 1234 5678 9123 0000"
+              />
+            </label>
+          </div>
+          <div>
+            <div>
+                <label htmlFor="month"><span>exp. date</span></label>
+                <input type="text" name="month" placeholder="MM"/>
+            </div>
+            <div>
+                <label htmlFor="year">
+                    <span>"(mm/yy)"</span>
+                    <input type="text" name="year" placeholder="YY"/>
+                </label>
+            </div>
+            <div>
+                <label htmlFor="cvc">
+                    <span>cvc</span>
+                    <input type="text" name="cvc" placeholder="e.g. 123"/>
+                </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit">Confirm</button>
+      </form>
+    </SC.CardDataCon>
+  );
 };
 
 export default CardData;
--- src/components/CardData/CardDataStyled.ts
+++ src/components/CardData/CardDataStyled.ts
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
 
 export const CardDataCon = styled.div`
-
+padding-top: 90px;
+padding-bottom: 44px;
+padding-left: 24px;
+padding-right: 24px;
 `
